a2b0b3e29198b23495705f4a84391ac6bfebd6df
Yusuke Izawa <yuizalp@gmail.com>
2018-02-16 21:24:32 +0900

    update

--- Basics.v
+++ Basics.v
@@ -202,3 +202,100 @@ Proof.
   - simpl. intros H. rewrite -> H. reflexivity.
 Qed.
 
+Theorem plus_0_r : forall n : nat, n + 0 = n.
+Proof.
+  intros n. induction n as [| n'].
+  Case "n = 0". reflexivity.
+  Case "n = S n". simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+
+Theorem minus_diag : forall n,
+    minus n n = 0.
+Proof.
+  intros n.
+  induction n as [| n'].
+  Case "n = 0".
+    simpl. reflexivity.
+  Case "n = S n'".
+  simpl. rewrite -> IHn'. reflexivity.
+Qed.
+
+Theorem mult_0_r : forall n : nat,
+    n * 0 = 0.
+Proof.
+  intros n.
+  induction n as [| n'].
+  Case "n = 0".
+  simpl.
+  reflexivity.
+  Case "n = S n'".
+  simpl.
+  rewrite -> IHn'.
+  reflexivity.
+Qed.
+
+Theorem plus_n_Sm : forall n m : nat,
+    S (n + m) = n + (S m).
+Proof.
+  intros n m.
+  induction n as [| n'].
+  Case "n = 0".
+  simpl.
+  reflexivity.
+  Case "n = S n'".
+  simpl.
+  rewrite -> IHn'.
+  reflexivity.
+Qed.
+
+Theorem plus_0 : forall n : nat,
+    n + 0 = n.
+Proof.
+  intros n.
+  induction n as [| n'].
+  Case "n = 0".
+  simpl.
+  reflexivity.
+  Case "n = S n'".
+  simpl.
+  rewrite -> IHn'.
+  reflexivity.
+Qed.
+
+
+Theorem plus_comm : forall n m : nat,
+    n + m = m + n.
+Proof.
+  intros n m.
+  induction n as [ | n'].
+  Case "n = 0".
+  simpl.
+  rewrite -> plus_0.
+  reflexivity.
+  Case "n = S n'".
+  simpl.
+  rewrite -> IHn'.
+  rewrite <- plus_n_Sm.
+  reflexivity.
+Qed.
+
+Fixpoint double (n : nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n.
+Proof.
+  intros n.
+  induction n as [ | n'].
+  Case "n = 0".
+  simpl.
+  reflexivity.
+  Case "n = S n'".
+  simpl.
+  rewrite -> IHn'.
+  rewrite -> plus_n_Sm.
+  reflexivity.
+Qed.
